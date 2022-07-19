2059a32b48851ffee3b80835c477a35f5ce8ac65
leandro alfonso <leandroalfonso645@gmail.com>
2022-07-19 00:21:07 -0300

    mi primer desafio de javascript

---
+++ main.js
@@ -0,0 +1,12 @@
+let nombre = prompt(`ingrese su nombre`)
+alert (`Hola ${nombre} vamos hacer una prueba de consola`)
+let numero = parseInt(prompt('Ingresa un numero: '))
+let palabra = prompt('Ingresa una palabra: ');
+
+for(let i=1; i<=numero; i++) {
+    console.log(`${i} : ${palabra}`)
+    
+}
+alert(`${nombre} dirígete a la consola donde veras ${numero} veces la palabra: ${palabra}`)
+
+
