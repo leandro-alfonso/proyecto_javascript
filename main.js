056013c31118c190c5d6b3fba6369307e076615c
leandro alfonso <leandroalfonso645@gmail.com>
2022-07-24 16:31:49 -0300

    segundo desafio simulador interactivo

--- main.js
+++ main.js
@@ -1,12 +1,62 @@
-let nombre = prompt(`ingrese su nombre`)
-alert (`Hola ${nombre} vamos hacer una prueba de consola`)
-let numero = parseInt(prompt('Ingresa un numero: '))
-let palabra = prompt('Ingresa una palabra: ');
+// empezamos dando nuestro dato
+let nombre = prompt(`Para comenzar ingrese su nombre`)
+alert (`Hola ${nombre.toUpperCase()} voy acompañarte a lo largo de tu compra`)
 
-for(let i=1; i<=numero; i++) {
-    console.log(`${i} : ${palabra}`)
-    
+// creo funciones
+function cañoCorrugado (){
+    let cantidad = parseInt(prompt(`ingrese la cantidad de rollos de caño corrugado estandar deseada`))
+    let total = cantidad * 1500
+    alert (`${nombre} tu total es $${total}`)
+
 }
-alert(`${nombre} dirígete a la consola donde veras ${numero} veces la palabra: ${palabra}`)
+function cable (){
+    let cantidad = parseInt(prompt(`ingrese la cantidad de rollos de cables estandar deseada`))
+    let total = cantidad * 2000
+    alert (`${nombre} tu total es $${total}`)
+}
+function termica (){
+    let cantidad = parseInt(prompt(`ingrese la cantidad de termica estandar deseada`))
+    let total = cantidad * 1000
+    alert (`${nombre} tu total es $${total}`)
+}
+function modulos (){
+    let cantidad = parseInt(prompt(`ingrese la cantidad de modulos estandar deseada`))
+    let total = cantidad * 500
+    alert (`${nombre} tu total es $${total}`)
+}
 
+function abrirMenu (menu){
+    let opcion = prompt (`Que elemento electrico desea comprar. \ \n1 .caño corrugado \n2 .cable \n3 .termica \n4 .modulos`)
+    return opcion
+}
+// seguimos con un menu de opciones 
+
+function elementos(){
+    let listaMenu = abrirMenu()
+    if (listaMenu == 1){
+        cañoCorrugado();
+        
+    }else if (listaMenu == 2){
+        cable();
+    }else if (listaMenu == 3){
+        termica()
+    }else if (listaMenu == 4) {
+        modulos()
+    }else {
+        alert (`selecciona una opcion valida`)
+    }
+
+}
+// terminamos con una funcion con ciclo que nos permita finalizar el proceso
+
+function continuar (){
+    let continuar = prompt (`deseas continuar?. *"NO" para terminar tu compra`)
+    while (continuar.toUpperCase() != "NO"){
+        elementos();
+        continuar = prompt (`deseas continuar?. *"NO" para terminar tu compra`);
+    }
+}
+// invoco funciones
+elementos();
+continuar();
 
